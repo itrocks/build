5d393eb725db8ca100be786f3160a7e91159c5e6
Baptiste Pillot <baptiste@pillot.fr>
2023-05-15 13:16:01 +0200

    ADD I16 Complete Replace unit tests

--- tests/Replace_Test.php
+++ tests/Replace_Test.php
@@ -12,15 +12,54 @@ class Replace_Test extends TestCase
 	//----------------------------------------------------------------------------------- DEFINITIONS
 	const DEFINITIONS = '<?php class Origin{} class Replacement{}';
 
-	//------------------------------------------------------------------- provideReplaceAttributeData
+	//--------------------------------------------------------------------------------- buildToBuffer
+	/**
+	 * @param string        $code If empty: __DIR__ . '/file.php' will be read
+	 * @param callable|null $init If set: call the callback Build init function with parameter Build
+	 */
+	protected function buildToBuffer(
+		string $code, array $configuration, bool $replaced, callable $init = null
+	) : string
+	{
+		$tokens = token_get_all($code);
+		$build  = new Build($configuration, new class(0, __DIR__) extends Index {
+			protected function prepareHome(): void { }
+		});
+		if ($tokens !== []) {
+			$build->class_index->file_tokens['file.php'] = $tokens;
+		}
+		if ($init !== null) {
+			call_user_func($init, $build);
+		}
+		$build->class_index->scanFile(__DIR__ . '/file.php');
+		$build->class_index->classify();
+		if ($tokens === []) {
+			$build->class_index->file_tokens = [];
+		}
+		$build->replace();
+		if ($replaced) {
+			static::assertArrayHasKey('file.php', $build->write_files);
+		}
+		else {
+			static::assertArrayNotHasKey('file.php', $build->write_files);
+		}
+		$buffer = '';
+		$tokens = $build->write_files['file.php'] ?? $build->class_index->file_tokens['file.php'];
+		foreach ($tokens as $token) {
+			$buffer .= is_string($token) ? $token : $token[1];
+		}
+		return $buffer;
+	}
+
+	//----------------------------------------------------------------------- provideReplaceCasesData
 	/** @return array<array{string,string,string}> */
-	public static function provideReplaceAttributeData() : array
+	public static function provideReplaceCasesData() : array
 	{
 		return [
 			[
 				'T_ARGUMENT-no',
 				'<?php function f(Origin $a, Lambda $l) {}',
-				'<?php function f(Origin $a, Lambda $l) {}',
+				'<?php function f(Origin $a, Lambda $l) {}'
 			],
 			[
 				'T_ATTRIBUTE',
@@ -35,7 +74,7 @@ class Replace_Test extends TestCase
 			[
 				'T_CLASS',
 				'<?php Origin::class; Lambda::class;',
-				'<?php \Replacement::class; Lambda::class;',
+				'<?php \Replacement::class; Lambda::class;'
 			],
 			[
 				'T_EXTENDS',
@@ -43,9 +82,19 @@ class Replace_Test extends TestCase
 				'<?php class C extends \Replacement, Lambda {}'
 			],
 			[
+				'T_EXTENDS_REPLACEMENT',
+				'<?php class Replacement extends Origin, Lambda {}',
+				'<?php class Replacement extends Origin, Lambda {}'
+			],
+			[
 				'T_NEW',
 				'<?php new Origin; new Lambda;',
-				'<?php new \Replacement; new Lambda;',
+				'<?php new \Replacement; new Lambda;'
+			],
+			[
+				'T_NEW_PARENTHESIS',
+				'<?php new Origin(15); new Lambda(18);',
+				'<?php new \Replacement(15); new Lambda(18);'
 			],
 			[
 				'T_STATIC',
@@ -55,7 +104,7 @@ class Replace_Test extends TestCase
 			[
 				'T_USE',
 				'<?php class C { use Origin, Lambda; }',
-				'<?php class C { use \Replacement, Lambda; }',
+				'<?php class C { use \Replacement, Lambda; }'
 			],
 			[
 				'T_DECLARE_CLASS-no',
@@ -75,23 +124,93 @@ class Replace_Test extends TestCase
 		];
 	}
 
-	//-------------------------------------------------------------------------- testReplaceAttribute
-	#[DataProvider('provideReplaceAttributeData')]
-	public function testReplaceAttribute(string $index, string $code, string $expected) : void
+	//------------------------------------------------------------------------------- testExcludeFile
+	public function testExcludeFile() : void
 	{
-		$tokens = token_get_all($code);
-		$build = new Build(['Origin' => 'Replacement'], new class(0, __DIR__) extends Index {
-			protected function prepareHome(): void {}
-		});
-		$build->class_index->file_tokens['file.php'] =& $tokens;
-		$build->class_index->scanFile(__DIR__ . '/file.php');
-		$build->class_index->classify();
-		$build->replace();
-		$buffer = '';
-		foreach ($tokens as $token) {
-			$buffer .= is_string($token) ? $token : $token[1];
+		$code     = '<?php new Origin';
+		$replaced = '<?php new \Replacement';
+		$cases    = [
+			$code     => function(Build $build) { $build->exclude_files['file.php'] = 0; },
+			$replaced => null
+		];
+		foreach ($cases as $expected => $init) {
+			$actual = $this->buildToBuffer($code, ['Origin' => 'Replacement'], !isset($init), $init);
+			static::assertEquals($expected, $actual, isset($init) ? 'exclusion' : 'no-exclusion');
 		}
-		static::assertEquals($expected, $buffer, $index);
+	}
+
+	//---------------------------------------------------------------------------------- testFromFile
+	public function testFromFile() : void
+	{
+		$code     = '<?php echo Origin::CONSTANT; echo Lambda::CONSTANT';
+		$expected = '<?php echo \Replacement::CONSTANT; echo Lambda::CONSTANT';
+		file_put_contents(__DIR__ . '/file.php', $code);
+		$actual = $this->buildToBuffer('', ['Origin' => 'Replacement'], true);
+		unlink(__DIR__ . '/file.php');
+		static::assertEquals($expected, $actual);
+	}
+
+	//------------------------------------------------------------------------------------ testNoSave
+	public function testNoSave() : void
+	{
+		/** @var Build $build */
+		$this->buildToBuffer(
+			'<?php $some_code;',
+			['Origin' => 'Replacement'],
+			false,
+			function(Build $source_build) use(&$build) { $build = $source_build; }
+		);
+		mkdir(__DIR__ . '/cache');
+		mkdir(__DIR__ . '/cache/build');
+		$build->save();
+		static::assertFalse(is_file(__DIR__ . '/cache/build/file.php'));
+		rmdir(__DIR__ . '/cache/build');
+		rmdir(__DIR__ . '/cache');
+	}
+
+	//------------------------------------------------------------------------------------- testNoUse
+	#[DataProvider('provideReplaceCasesData')]
+	public function testNoUse(string $index, string $code) : void
+	{
+		$actual = $this->buildToBuffer($code, ['Maybe' => 'Not'], false);
+		static::assertEquals($code, $actual, $index);
+	}
+
+	//------------------------------------------------------------------------------ testReplaceCases
+	#[DataProvider('provideReplaceCasesData')]
+	public function testReplaceCases(string $index, string $code, string $expected) : void
+	{
+		$actual = $this->buildToBuffer($code, ['Origin' => 'Replacement'], $expected !== $code);
+		static::assertEquals($expected, $actual, $index);
+	}
+
+	//-------------------------------------------------------------------------- testReplacementArray
+	public function testReplacementArray() : void
+	{
+		$actual = $this->buildToBuffer(
+			'<?php namespace N; new Origin;', ['N\Origin' => ['Trait']], true
+		);
+		$expected = '<?php namespace N; new \N\Origin\B;';
+		static::assertEquals($expected, $actual);
+	}
+
+	//-------------------------------------------------------------------------------------- testSave
+	public function testSave() : void
+	{
+		/** @var Build $build */
+		$actual = $this->buildToBuffer(
+			'<?php new Origin();',
+			['Origin' => 'Replacement'],
+			true,
+			function(Build $source_build) use(&$build) { $build = $source_build; }
+		);
+		mkdir(__DIR__ . '/cache');
+		mkdir(__DIR__ . '/cache/build');
+		$build->save();
+		static::assertTrue(is_file(__DIR__ . '/cache/build/file'));
+		unlink(__DIR__ . '/cache/build/file');
+		rmdir(__DIR__ . '/cache/build');
+		rmdir(__DIR__ . '/cache');
 	}
 
 }
