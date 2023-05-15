eee6f7f14f91fe3e703636ec0e094c2b4cd7aa2a
Baptiste Pillot <baptiste@pillot.fr>
2023-05-15 12:20:40 +0200

    ADD I16 Replace uses as attribute parameter, exclude Extend and Implement + REF separate save() operation

--- src/Replace.php
+++ src/Replace.php
@@ -1,11 +1,17 @@
 <?php
 namespace ITRocks\Build;
 
+use ITRocks\Extend;
+
 trait Replace
 {
 
+	//------------------------------------------------------------------------ EXCLUDE_ATTRIBUTE_USES
+	/** @var string[] Class uses into these attributes are not replaced */
+	protected const EXCLUDE_ATTRIBUTE_USES = [Extend::class, Implement::class];
+
 	//-------------------------------------------------------------------------------- REPLACED_TYPES
-	/** @var int[] */
+	/** @var int[] Only these class use types are replaced */
 	protected const REPLACED_TYPES = [T_ATTRIBUTE, T_CLASS, T_EXTENDS, T_NEW, T_STATIC, T_USE];
 
 	//-------------------------------------------------------------------------------- $exclude_files
@@ -23,46 +29,54 @@ trait Replace
 	/** Replace references to original classes by reference to the replacement class */
 	public function replace() : void
 	{
+		$search = [];
 		foreach ($this->configuration as $class => $replacement) {
-			foreach (static::REPLACED_TYPES as $search[T_TYPE]) {
-				$search[T_USE] = $class;
-				$class_uses    = $this->class_index->search($search, true);
-				if ($class_uses === []) {
+			$search[T_USE] = $class;
+			$class_uses    = $this->class_index->search($search, true);
+			if ($class_uses === []) {
+				continue;
+			}
+			if (is_array($replacement)) {
+				$replacement = $class . '\\B';
+			}
+			foreach ($class_uses as $class_use) {
+				$type = $class_use[T_TYPE];
+				if (
+					(is_int($type) && !in_array($type, static::REPLACED_TYPES, true))
+					|| (is_string($type) && in_array($type, static::EXCLUDE_ATTRIBUTE_USES, true))
+				) {
 					continue;
 				}
-				if (is_array($replacement)) {
-					$replacement = $class . '\\B';
+				if (($type === T_EXTENDS) && in_array($class_use[T_CLASS], $this->configuration, true)) {
+					continue;
 				}
-				foreach ($class_uses as $class_use) {
-					if (
-						($search[T_TYPE] === T_EXTENDS)
-						&& in_array($class_use[T_CLASS], $this->configuration, true)
-					) {
-						continue;
-					}
-					/** @var string $file phpstan fault */
-					$file = $class_use[T_FILE];
-					if (isset($this->exclude_files[$file])) {
-						continue;
-					}
-					if (!isset($this->class_index->file_tokens[$file])) {
-						$file_content = file_get_contents($file);
-						if ($file_content === false) $file_content = '';
-						$this->class_index->file_tokens[$file] = token_get_all($file_content);
-					}
-					if (!isset($this->write_files[$file])) {
-						$this->write_files[$file] =& $this->class_index->file_tokens[$file];
-					}
-					/** @var int $line phpstan fault */
-					$line = $class_use[T_LINE];
-					$this->write_files[$file][$class_use[T_TOKEN_KEY]] = [
-						T_NAME_FULLY_QUALIFIED,
-						'\\' . $replacement,
-						$line
-					];
+				/** @var string $file phpstan fault */
+				$file = $class_use[T_FILE];
+				if (isset($this->exclude_files[$file])) {
+					continue;
 				}
+				if (!isset($this->class_index->file_tokens[$file])) {
+					$file_content = file_get_contents($file);
+					if ($file_content === false) $file_content = '';
+					$this->class_index->file_tokens[$file] = token_get_all($file_content);
+				}
+				if (!isset($this->write_files[$file])) {
+					$this->write_files[$file] =& $this->class_index->file_tokens[$file];
+				}
+				/** @var int $line phpstan fault */
+				$line = $class_use[T_LINE];
+				$this->write_files[$file][$class_use[T_TOKEN_KEY]] = [
+					T_NAME_FULLY_QUALIFIED,
+					'\\' . $replacement,
+					$line
+				];
 			}
 		}
+	}
+
+	//------------------------------------------------------------------------------------------ save
+	public function save() : void
+	{
 		foreach ($this->write_files as $file => $tokens) {
 			$buffer = '';
 			foreach ($tokens as $token) {
